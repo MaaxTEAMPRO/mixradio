1829280175382bc93ab4e6b1651b5ae958611d8e
Maax TEAMPRO <73242958+MaaxTEAMPRO@users.noreply.github.com>
2024-10-01 19:56:56 -0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,6 @@
+// links.js
+const links = [
+    { name: 'Google', url: 'https://www.google.com', icon: 'icon_links/google.png' },
+    { name: 'YouTube', url: 'https://www.youtube.com', icon: 'icon_links/youtube.png' },
+    { name: 'OpenAI', url: 'https://www.openai.com', icon: 'icon_links/openai.png' }
+];
---
+++ stations.js
@@ -0,0 +1,3 @@
+const stations = [
+    { name: 'Love Radio - Timeless Songs', url: 'https://stream.btsstream.com:8092/loveradio.aac', icon:'icons/Love_Radio_Timeless_Songs.png'}
+];
