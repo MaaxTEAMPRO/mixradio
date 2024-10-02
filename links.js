81adcc3831fb38a7b91ee26e2e876c3aa2acf595
Maax TEAMPRO <73242958+MaaxTEAMPRO@users.noreply.github.com>
2024-10-01 23:54:26 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
 // links.js
 const links = [
-    { name: 'Sala anos 80s', url: 'https://www.google.com', icon: 'icon_links/avakin1.jpg' },
+    { name: 'Sala Gold 80s', url: 'https://get.avakin.com/WxfdUKs6lNb', icon: 'icon_links/avakin1.jpg' },
     { name: 'YouTube', url: 'https://www.youtube.com', icon: 'icon_links/youtube.png' },
     { name: 'OpenAI', url: 'https://www.openai.com', icon: 'icon_links/openai.png' }
 ];
