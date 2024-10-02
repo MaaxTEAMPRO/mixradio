41291faa1d62c14b21e338a449e0d010b4ea6230
MaaxTEAMPRO <22edirlei@gmail.com>
2024-10-02 18:07:15 -0300

    a

--- stations.js
+++ stations.js
@@ -1,9 +1,20 @@
-// Definindo variáveis agrupadas em um objeto
-const station1 = {
-    name: 'Love Radio - Timeless Song',
-    url: 'https://stream.btsstream.com:8092/loveradio.aac',
-    icon: 'icons/Love_Radio_Timeless_Songs.png'
-};
+const stations = [
+    {
+        name: 'Love Radio - Timeless Songs',
+        url: 'https://stream.btsstream.com:8092/loveradio.aac',
+        icon: 'icons/Love_Radio_Timeless_Songs.png'
+    },
+    {
+        name: 'Classic Hits Radio',
+        url: 'https://stream.classichits.com/stream.aac',
+        icon: 'icons/Classic_Hits_Radio.png'
+    },
+    {
+        name: 'Jazz Radio',
+        url: 'https://stream.jazzradio.com/jazz.aac',
+        icon: 'icons/Jazz_Radio.png'
+    }
+];
 
-// Usando o objeto para criar o array de estações
-const stations = [station1];
+// Exemplo de impressão do array de estações no console
+console.log(stations);
