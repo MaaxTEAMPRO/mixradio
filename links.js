b5576e772cc4bc9f08ba61d29c34e494053c9487
MaaxTEAMPRO <22edirlei@gmail.com>
2024-10-01 23:21:03 -0300

    teste sala

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
 // links.js
 const links = [
-    { name: 'Google', url: 'https://www.google.com', icon: 'icon_links/google.png' },
+    { name: 'Sala anos 80s', url: 'https://www.google.com', icon: 'avakin1.jpg' },
     { name: 'YouTube', url: 'https://www.youtube.com', icon: 'icon_links/youtube.png' },
     { name: 'OpenAI', url: 'https://www.openai.com', icon: 'icon_links/openai.png' }
 ];
