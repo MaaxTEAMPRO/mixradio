c2c0d95cabc75d8d51ee014686b28a1fb3700546
MaaxTEAMPRO <22edirlei@gmail.com>
2024-10-01 23:22:15 -0300

    a

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
 // links.js
 const links = [
-    { name: 'Sala anos 80s', url: 'https://www.google.com', icon: 'avakin1.jpg' },
+    { name: 'Sala anos 80s', url: 'https://www.google.com', icon: 'icon_links/avakin1.jpg' },
     { name: 'YouTube', url: 'https://www.youtube.com', icon: 'icon_links/youtube.png' },
     { name: 'OpenAI', url: 'https://www.openai.com', icon: 'icon_links/openai.png' }
 ];
